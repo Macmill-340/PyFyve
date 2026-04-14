ee2cbcb8c78823ce69fc51c28fc3f8dc087d7ebe
Macmill <captainmacmill340@gmail.com>
2026-04-14 08:48:22 +0530

    fixed console output

--- src/load_progress.py
+++ src/load_progress.py
@@ -1,26 +1,27 @@
 import json
 import os
+from console import console
 
-default_file = "user_progress.json"
+DEFAULT_PROGRESS_FILE = "user_progress.json"
 
-def load_progress(filename = default_file):
-    if not(os.path.isfile(filename)) or os.path.getsize(filename) == 0:
-        save_progress(0)
+
+def load_progress(filename=DEFAULT_PROGRESS_FILE):
+    if not os.path.isfile(filename) or os.path.getsize(filename) == 0:
+        save_progress(0, filename)
         return 0
 
     try:
         with open(filename, "r") as f:
-            finished_lesson = json.load(f)
-            return finished_lesson.get("finished_lesson_index", 0)
+            return json.load(f).get("finished_lesson_index", 0)
     except Exception as e:
-        print(f"Error loading progress: {e}, Resetting file")
-        save_progress(0)
+        console.print(f"Error loading progress: {e} — resetting file.", style="warning")
+        save_progress(0, filename)
         return 0
 
-def save_progress(progress, filename = default_file):
-    data = {"finished_lesson_index": progress}
+
+def save_progress(progress, filename=DEFAULT_PROGRESS_FILE):
     try:
         with open(filename, "w") as f:
-            json.dump(data, f, indent=4)
+            json.dump({"finished_lesson_index": progress}, f, indent=4)
     except Exception as e:
-        print(f"Error saving progress: {e}")
+        console.print(f"Error saving progress: {e}", style="warning")
